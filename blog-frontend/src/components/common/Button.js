0ed20c6a1fbeda98a8673086ad1c0001e34ae29d
Pridesd <whwotjr98@naver.com>
2022-03-19 17:10:13 +0900

    Header component

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,7 +1,8 @@
 import styled, {css} from 'styled-components';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styled/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -31,8 +32,18 @@ const StyledButton = styled.button`
                 background: ${palette.cyan[4]};
             }
         `}
+`
+
+const StyledButton = styled.button`
+  ${buttonStyle}
 `;
 
-const Button = (props) => <StyledButton {...props} />;
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`;
 
+const Button = (props) =>{ 
+  return props.to ? (<StyledLink {...props} cyan={props.cyan? 1: 0} />):
+  (<StyledButton {...props} />)
+}
 export default Button;
